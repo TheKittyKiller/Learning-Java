c83f07a9ac7caf8387e22776d51e08e2f9f1697e
sebastian melph <jame1307@stud.kea.dk>
2023-05-07 15:14:58 +0200

    boolean

---
+++ ifandelse.js
@@ -0,0 +1,19 @@
+
+
+
+const age = 18;
+
+if (age >= 16) {
+
+    console.log("man can whip")
+    console.log('goodshit')
+} else if (age >= 14) {
+
+console.log('shit fam just one moore year inint')
+
+} else {
+
+console.log('man cant whip init')
+
+
+}

b454ddeb4b62aef369f4dcd4da3dbb6a375c163e
sebastian melph <jame1307@stud.kea.dk>
2023-05-07 15:15:37 +0200

    boolean

--- ifandelse.js
+++ ifandelse.js
@@ -6,7 +6,7 @@ const age = 18;
 if (age >= 16) {
 
     console.log("man can whip")
-    console.log('goodshit')
+    console.log('goodstuff')
 } else if (age >= 14) {
 
 console.log('shit fam just one moore year inint')
